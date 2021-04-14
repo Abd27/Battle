a1aa6c23ee21f122b14ed021f78f9bc5b5e1b533
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-14 15:31:32 +0100

    POST/redirect/GET pattern complete

--- app.rb
+++ app.rb
@@ -2,25 +2,25 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
-  # get '/' do
-  #   'Hello Battle'
-  # end
-
   get '/' do
-    # <form>
-    #   <input type="text" name="player_1_name">
-    # </form>
     erb(:index)  
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
