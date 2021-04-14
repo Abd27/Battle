6c4b7c02c1c39d5fc3ee81e8e41abf1725f78031
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-14 16:03:38 +0100

    big refactor and test helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Finn'
-    fill_in :player_2_name, with: 'Abdur'
-    click_button 'Submit'
+    sign_in_and_play
     save_and_open_page
     expect(page).to have_content 'Finn vs Abdur'
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit points' do
   scenario 'see player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Finn'
-    fill_in :player_2_name, with: 'Abdur'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Abdur: 60 HP'
   end
 end
